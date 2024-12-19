e571bb515d92ac2fe00b2762d89dba66a407e6ad
Fabien Sanglier <Fabien.Sanglier@ibm.com>
2024-12-19 00:27:02 -0500

    many more update with the SDK script to enhance robustness

--- sdk-scripts/streamsets_techzone_automation.py
+++ sdk-scripts/streamsets_techzone_automation.py
@@ -59,7 +59,8 @@ deployment_engine_version = '6.0.0'
 deployment_engine_type = 'DC'
 deployment_tags = ['k3s-techzone-sdc-6.0.0','kafka','postgres','minio',"s3"]
 deployment_external_resource_location = 'http://minio.minio.svc.cluster.local/streamsets-deployment-archives/externalResources-demos.zip'
-deployment_stage_libs = [
+
+deployment_stage_libs_min = [
 	"aws:6.0.0",
 	"apache-kafka_3_7:6.0.0",
 	"basic:6.0.0",
@@ -69,13 +70,49 @@ deployment_stage_libs = [
 	"jdbc:6.0.0",
 	"jython_2_7:6.0.0"
 ]
+
+deployment_stage_libs_extensive = [
+	"kinesis:6.0.0",
+	"aws:6.0.0",
+	"apache-kafka_3_7:6.0.0",
+	"aws-secrets-manager-credentialstore:6.0.0",
+	"azure-keyvault-credentialstore:6.0.0",
+	"azure:6.0.0",
+	"basic:6.0.0",
+	"crypto:6.0.0",
+	"cyberark-credentialstore:6.0.0",
+	"dataformats:6.0.0",
+	"sdc-databricks:6.0.0",
+	"dev:6.0.0",
+	"elasticsearch_8:6.0.0",
+	"bigtable:6.0.0",
+	"google-cloud:6.0.0",
+	"google-secret-manager-credentialstore:6.0.0",
+	"groovy_4_0:6.0.0",
+	"jks-credentialstore:6.0.0",
+	"jdbc:6.0.0",
+	"jms:6.0.0",
+	"jython_2_7:6.0.0",
+	"webclient-impl-okhttp:6.0.0",
+	"orchestrator:6.0.0",
+	"redis:6.0.0",
+	"salesforce:6.0.0",
+	"singlestore:6.0.0",
+	"sdc-snowflake:6.0.0",
+	"vault-credentialstore:6.0.0"
+]
+
+## pick the extensive one for more coverage for demos
+deployment_stage_libs = deployment_stage_libs_extensive
+
 deployment_kub_labels = {'environment': 'techzone'}
 
 ############################# functions
 
-# Method to add a user to an Organization
-def deploy_k8s_agent(kubeconfig_path, install_yaml, install_namespace):
-    print("Deploying Kubernetes YAML")
+def kube_deploy_yaml(kubeconfig_path, namespace_name, install_yaml):
+    """Patching Kubernetes deployments's dnspolicy"""
+
+    print("Deploying Kubernetes YAML into namespace {}, with kube config {}".format(namespace_name, kubeconfig_path))
     
     # Load Kubernetes configuration
     config.load_kube_config(config_file=kubeconfig_path)
@@ -90,12 +127,76 @@ def deploy_k8s_agent(kubeconfig_path, install_yaml, install_namespace):
     install_yaml_objects = yaml.safe_load_all(install_yaml)
 
     # Create the Kubernetes objects from the YAML string
-    utils.create_from_yaml(api_client, yaml_objects=install_yaml_objects, namespace=install_namespace, verbose=True)
+    utils.create_from_yaml(api_client, yaml_objects=install_yaml_objects, namespace=namespace_name, verbose=True)
 
     print("Agent Deployment created!")
 
 
-def wait_for_state(check_function, desired_state, timeout=10, interval=1):
+def kube_patch_deployment_dnspolicy(kubeconfig_path, namespace_name, deployments):
+    """Patching Kubernetes deployments's dnspolicy"""
+
+    # Load Kubernetes configuration
+    config.load_kube_config(config_file=kubeconfig_path)
+
+    # Create a Kubernetes API client
+    api_instance = client.AppsV1Api()
+
+    # Create a patch object
+    patch = {
+        "spec": {
+            "template": {
+                "spec": {
+                    "dnsPolicy": "ClusterFirst" 
+                }
+            }
+        }
+    }
+
+    # Patch all the pod
+    for dep in deployments:
+        dep_name = dep.metadata.name
+        print(dep_name)
+        try:
+            api_instance.patch_namespaced_deployment(dep_name, namespace_name, patch)
+            print("Deployment DNS policy patched successfully.")
+        except client.rest.ApiException as e:
+            print(f"Error patching pod: {e}")
+
+def kube_get_pods_by_label(kubeconfig_path, namespace_name, label_selector):
+    """Gets a pod by label in the specified namespace."""
+
+    # Load Kubernetes configuration
+    config.load_kube_config(config_file=kubeconfig_path)
+
+    # Create a Kubernetes API client
+    api_instance = client.CoreV1Api()
+
+    # List pods in the specified namespace, filtered by label
+    pods = api_instance.list_namespaced_pod(namespace=namespace_name, label_selector=label_selector)
+
+    if pods.items:
+        return pods.items
+    else:
+        return []
+
+def kube_get_deployments_by_label(kubeconfig_path, namespace_name, label_selector):
+    """Gets a pod by label in the specified namespace."""
+
+    # Load Kubernetes configuration
+    config.load_kube_config(config_file=kubeconfig_path)
+
+    # Create a Kubernetes API client
+    api_instance = client.AppsV1Api()
+
+    # List deployments in the specified namespace, filtered by label
+    deployments = api_instance.list_namespaced_deployment(namespace=namespace_name, label_selector=label_selector)
+
+    if deployments.items:
+        return deployments.items
+    else:
+        return []
+    
+def wait_for_state(check_function, desired_state, desired_state_not=False, timeout=10, interval=1):
     """Waits for a condition to become true.
 
     Args:
@@ -112,8 +213,13 @@ def wait_for_state(check_function, desired_state, timeout=10, interval=1):
     while True:
         print('Waiting...')
         current_state = check_function()
-        if current_state == desired_state:
-            return True
+
+        if desired_state_not:
+            if current_state != desired_state:
+                return True
+        else:
+            if current_state == desired_state:
+                return True
 
         if time.time() - start_time > timeout:
             print('Reached timeout...')
@@ -126,6 +232,14 @@ class PwdAction(argparse.Action):
          mypass = getpass.getpass()
          setattr(namespace, self.dest, mypass)
 
+def get_environment_status(env_name):
+    env_check = sch.environments.get(environment_name=env_name)
+    return env_check.agent_status
+
+def get_deployment_status(dep_name):
+    dep_check = sch.deployments.get(deployment_name=dep_name)
+    return dep_check.state
+
 ############################# end functions
 
 # Add named arguments
@@ -133,7 +247,7 @@ parser = argparse.ArgumentParser(description="StreamSets Techzone Automation scr
 parser.add_argument("-u", "--cred_id", help="StreamSets Control Hub API ID (Optional: Also set via OS ENV var 'SCH_CRED_ID)'", default=os.environ.get('SCH_CRED_ID'))
 parser.add_argument('-p', "--cred_token", action=PwdAction, nargs=0, dest='cred_token', help="StreamSets Control Hub API Token (Optional: Also set via OS ENV var 'SCH_CRED_TOKEN)'", default=os.environ.get('SCH_CRED_TOKEN'))
 parser.add_argument("-kn", "--kube_nodeploy", action='store_true', help="Deploy the agent YAML or not")
-parser.add_argument("-kc", "--kube_config", help="Kube Config path", default="~/.kube/config")
+parser.add_argument("-kc", "--kube_config", help="Kube Config path", default=os.environ.get('KUBECONFIG'))
 parser.add_argument("-s", "--suffix", help="Suffix to add to the created objects", default=os.environ.get('SCH_OBJ_SUFFIX'))
 
 # Parse the arguments
@@ -212,15 +326,19 @@ except ValueError:
 
 print("INFO: environment state = {} / status = {} / status details = {}".format(environment.state, environment.agent_status, environment.agent_status_detail))
 
+if environment.state == 'DEACTIVATED':
+    print('Environment in DEACTIVATED state - RE-ACTIVATING!')
+    sch.activate_environment(environment, timeout_sec=300)
+
 if environment.state != 'ACTIVE':
     raise ValueError("Environment should be activated at this point")
 
 if not kube_nodeploy:
     try:
-        if environment.agent_status != 'ONLINE':
+        if get_environment_status(env_name_final) != 'ONLINE':
             print("Environment agent is not online...installing it!")
             install_yaml = sch.get_kubernetes_environment_yaml(environment)
-            deploy_k8s_agent(kube_config_path, install_yaml, env_kub_namespace)
+            kube_deploy_yaml(kube_config_path, env_kub_namespace, install_yaml)
 
     except Exception as e:
         print('Error creating the kubernetes artifatcs automatically!')
@@ -229,12 +347,8 @@ if not kube_nodeploy:
         print(install_script)
         print(str(e))
 
-
     print("Waiting a bit for Environment ONLINE before next step")
-    def get_environment_status():
-        return environment.agent_status
-
-    if wait_for_state(get_environment_status, 'ONLINE', timeout=60):
+    if wait_for_state(lambda: get_environment_status(env_name_final), desired_state='ONLINE', timeout=60, interval=5):
         print("Environment is ONLINE!!")
 
 ################################ deployment section - create new or get
@@ -280,13 +394,36 @@ except ValueError:
         print(str(e))
         sys.exit(1)
 
-# Optional - equivalent to clicking on 'Launch Deployment'
-print('Deployment state = {}'.format(deployment.state))
-if deployment.state != 'ACTIVE':
+# Equivalent to clicking on 'Launch Deployment'
+dep_state = get_deployment_status(deployment_name_final)
+print('Deployment state = {}'.format(dep_state))
+
+if dep_state == 'ACTIVATING':
+    if wait_for_state(lambda: get_deployment_status(deployment_name_final), desired_state='ACTIVATING', desired_state_not=True, timeout=60, interval=5):
+        print("Environment is NOT ACTIVATING anymore!!")
+
+if dep_state == 'DEACTIVATING':
+    if wait_for_state(lambda: get_deployment_status(deployment_name_final), desired_state='DEACTIVATING', desired_state_not=True, timeout=60, interval=5):
+        print("Environment is NOT DEACTIVATING anymore!!")
+
+if dep_state != 'ACTIVE':
     try:
-        print('Deployment not active...Starting the deployment!')
+        print('Deployment not active...re-starting the deployment in case it was in error state...')
+        sch.stop_deployment(deployment)
         sch.start_deployment(deployment)
     except Exception as e:
         print(str(e))
+
+if dep_state != 'ACTIVE':
+    if wait_for_state(lambda: get_deployment_status(deployment_name_final), desired_state='ACTIVE', timeout=60, interval=5):
+        print("Deployment is ACTIVE!!")
+    else:
+        raise ValueError("Deployment should be activated at this point")
+
+## finally, patch the pods
+print("Patch the dnspolicy of all the deployments in the namespace {} due to manifest issues".format(env_kub_namespace))
+label_selector = "environment=techzone"
+deployments = kube_get_deployments_by_label(kube_config_path, env_kub_namespace, label_selector)
+kube_patch_deployment_dnspolicy(kube_config_path, env_kub_namespace, deployments)
 
 print('Done')
